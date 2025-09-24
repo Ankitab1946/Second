28ad6bd7a6e8502586d168899ede2b28fc2835ce
Ankitab1946 <96777490+Ankitab1946@users.noreply.github.com>
2025-09-24 12:35:37 +0530

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,7 @@
+
+
+# from the project root
+type NUL > app/__init__.py        # Windows
+type NUL > features/__init__.py
+type NUL > features/steps/__init__.py
+# (macOS/Linux: touch app/__init__.py features/__init__.py features/steps/__init__.py)
