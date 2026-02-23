448d9026d1d28a7125f1b0423ba360c887ba9243
Ankitab1946 <96777490+Ankitab1946@users.noreply.github.com>
2026-02-23 22:04:23 +0530

    Update charts.py

--- JiraReporting/charts.py
+++ JiraReporting/charts.py
@@ -1,30 +1,27 @@
 import plotly.express as px
 
+def efficiency_chart(df):
+    return px.bar(
+        df,
+        x="user",
+        y="efficiency",
+        title="SP vs Hours Efficiency"
+    )
 
-def bar_assigned_vs_completed(df):
+def velocity_chart(df):
+    return px.line(
+        df,
+        x="sprint",
+        y="completed_sp",
+        markers=True,
+        title="Velocity Trend"
+    )
+
+def commitment_snapshot(df):
     return px.bar(
         df,
         x="user",
         y=["assigned_sp", "completed_sp"],
         barmode="group",
-        title="Assigned vs Completed Story Points"
-    )
-
-
-def stacked_spillover(df):
-    return px.bar(
-        df,
-        x="user",
-        y=["completed_sp", "spillover_sp"],
-        barmode="stack",
-        title="Completed vs Spillover"
-    )
-
-
-def pie_sp_distribution(df):
-    return px.pie(
-        df,
-        values="assigned_sp",
-        names="user",
-        title="Story Point Distribution"
+        title="Committed vs Completed SP"
     )
