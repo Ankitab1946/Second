037604dcb6c7a77f8c94f088e71a0d8cea634dee
Ankitab1946 <96777490+Ankitab1946@users.noreply.github.com>
2026-02-23 15:28:02 +0530

    Create charts.py

---
+++ JiraReporting/charts.py
@@ -0,0 +1,30 @@
+import plotly.express as px
+
+
+def bar_assigned_vs_completed(df):
+    return px.bar(
+        df,
+        x="assignee",
+        y=["assigned_sp", "completed_sp"],
+        barmode="group",
+        title="Assigned vs Completed Story Points"
+    )
+
+
+def stacked_spillover(df):
+    return px.bar(
+        df,
+        x="assignee",
+        y=["completed_sp", "spillover_sp"],
+        barmode="stack",
+        title="Committed vs Spillover"
+    )
+
+
+def pie_sp_distribution(df):
+    return px.pie(
+        df,
+        values="assigned_sp",
+        names="assignee",
+        title="Story Point Distribution"
+    )
